dc823aadfa5311f8730cb007628fe7b0574fe90a
DayVee-Programming <dreyimbaev02@gmail.com>
2025-12-01 20:19:19 +0500

    feat(packages): add '@vercel/analytics' package

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import NotFoundPage from "@/pages/NotFoundPage.tsx";
 import DefaultPage from "@/pages/DefaultPage";
 import HomeContent from "./components/contents/HomeContent";
+import { Analytics } from "@vercel/analytics/react"
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,7 @@ createRoot(document.getElementById("root")!).render(
       <RouterProvider router={router} />
 
       <ToastContainer />
+      <Analytics/>
     </AppContextProvider>
   </StrictMode>
 );
